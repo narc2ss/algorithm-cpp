b2ddaf61eae48ce3fb5105d2d971591d5cdc2771
narc2ss <private.narc2ss@gmail.com>
2022-04-29 18:32:23 +0900

    feat : 플로이드-와샬 알고리즘을 구현한다

---
+++ graph/floyd-warchall/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int number = 4;
+int INF = 1000000000;
+
+int a[4][4] = {
+    {0, 5, INF, 8},
+    {7, 0, 9, INF},
+    {2, INF, 0, 4},
+    {INF, INF, 3, 0},
+};
+
+void floyd_warchall() {
+  int d[number][number];
+
+  for (int i = 0; i < number; i++) {
+    for (int j = 0; j < number; j++) {
+      d[i][j] = a[i][j];
+    }
+  }
+
+  for (int k = 0; k < number; k++) {
+    for (int i = 0; i < number; i++) {
+      for (int j = 0; j < number; j++) {
+        if (d[i][k] + d[k][j] < d[i][j]) {
+          d[i][j] = d[i][k] + d[k][j];
+        }
+      }
+    }
+  }
+
+  for (int i = 0; i < number; i++) {
+    for (int j = 0; j < number; j++) {
+      cout << d[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
+int main(void) {
+  floyd_warchall();
+}
